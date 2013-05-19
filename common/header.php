b68ee07b24c004736c25ba0bf54df2bc3961a7f6
Pete <thewub.wiki@googlemail.com>
2013-05-19 22:13:37 +0100

    fix responsive bootstrap

--- common/header.php
+++ common/header.php
@@ -13,7 +13,7 @@ if(!empty($title)) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!-- Bootstrap -->
         <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
-        <link href="bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
+        <link href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
         <!-- custom css -->
         <link href="main.css" rel="stylesheet">
         <link rel="icon" href="favicon.ico" type="image/x-icon" />
